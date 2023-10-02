38683869f29ae1aab851c4535be6c74605b46ce9
Jihoon_Song <69960711+HelloTech69@users.noreply.github.com>
2023-10-02 14:05:14 +0800

    config for nginx and self signed cert

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -2,12 +2,10 @@ const checkAuthentication = (req, res, next) => {
     // Check if the user is authenticated based on the session
     
     if (req.session.user) {
-      console.log('User is authenticated')
       // User is authenticated, call next to continue processing the request
       next();
     } else {
       // User is not authenticated, send a response with unauthorized status
-      console.log('User is not authenticated')
       res.status(401).json({ authenticated: false });
     }
   };
