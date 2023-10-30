be2d16857639a5465c3c7abb6c7d2e81bdb9c2a3
keithtch <136182973+keithtch@users.noreply.github.com>
2023-10-30 23:41:53 +0800

    editing homepage

--- client/src/app/private/homepage/Homepage.jsx
+++ client/src/app/private/homepage/Homepage.jsx
@@ -1,16 +1,16 @@
 import {react} from 'react';
 import '../../../assets/styles/private_styles/Homepage.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Carousel, Card, Row, Col } from 'react-bootstrap';
+import Image from 'react-bootstrap/Image';
+import nutritionShareImage from './images/Nutrition_share.jpg'
 
 export default function Home() {
     return (
         <div className='col-lg-9 container bg-light homepage-container'>
             <div className="container w-100 h-100">
-                <div className="row h-100 align-items-center justify-content-center text-center">
-                    <div className="col-lg-8">
-                        <h1 className="font-weight-light">Welcome to the Health Care App</h1>
-                        <p className="lead text-muted">This is a application which helps you to manage your diet, track your daily calories and much more</p>
-                    </div>
-                </div>
+                {/* edit here */}
+                {/* edit ends here. */}
             </div>
         </div>
     )
