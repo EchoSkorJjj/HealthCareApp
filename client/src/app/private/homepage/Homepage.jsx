de417fb8da1c2253b5a6a2108a968c4997e6fce4
keithtch <136182973+keithtch@users.noreply.github.com>
2023-10-30 23:55:37 +0800

    Carousell for the features

--- client/src/app/private/homepage/Homepage.jsx
+++ client/src/app/private/homepage/Homepage.jsx
@@ -4,13 +4,55 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Carousel, Card, Row, Col } from 'react-bootstrap';
 import Image from 'react-bootstrap/Image';
 import nutritionShareImage from './images/Nutrition_share.jpg'
+import Exercise from './images/exercise1.jpg'
+import Recipe from './images/recipe_analyis.jpg'
+import Charts from './images/charts.jpg'
 
 export default function Home() {
     return (
         <div className='col-lg-9 container bg-light homepage-container'>
             <div className="container w-100 h-100">
-                {/* edit here */}
-                {/* edit ends here. */}
+{/* edit here */}
+                <div>
+                    <Container>
+                        <Carousel fade>
+                            <Carousel.Item interval={5000}>
+                                <img className="d-block w-100" src={nutritionShareImage} alt="First slide" />
+                            <Carousel.Caption>
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </Carousel.Caption>
+                            </Carousel.Item>
+
+                            <Carousel.Item interval={5000}>
+                                <img className="d-block w-100" src={Recipe} alt="First slide" />
+                            <Carousel.Caption>
+                                <h5>Second slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </Carousel.Caption>
+                            </Carousel.Item>
+
+                            <Carousel.Item interval={5000}>
+                                <img className="d-block w-100" src={Exercise} alt="First slide" />
+                            <Carousel.Caption>
+                                <h5>Third slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </Carousel.Caption>
+                            </Carousel.Item>
+
+                            <Carousel.Item interval={5000}>
+                                <img className="d-block w-100" src={Charts} alt="First slide" />
+                            <Carousel.Caption>
+                                <h5>Fourth slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </Carousel.Caption>
+                            </Carousel.Item>
+          {/* Add more Carousel items here */}
+                         </Carousel>
+                    </Container>
+                         <br />
+                </div>
+{/* edit ends here. */}
             </div>
         </div>
     )
