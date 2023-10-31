c35dd0d522025d48907bb6cd3ecbda0da929e2e5
Jihoon_Song <69960711+HelloTech69@users.noreply.github.com>
2023-10-31 23:22:29 +0800

    test

--- client/src/app/public/login/Login.jsx
+++ client/src/app/public/login/Login.jsx
@@ -65,8 +65,6 @@ export default function Login() {
       onSuccess: async ({ code }) => {
         try {
           const baseUrl = import.meta.env.VITE_NODE_ENV === 'production' ? import.meta.env.VITE_HTTPS_SERVER : import.meta.env.VITE_DEVELOPMENT_SERVER;
-          console.log(import.meta.env.VITE_NODE_ENV)
-          console.log(baseUrl);
           const response = await axios.post(`${baseUrl}/api/auth/google/callback`, {
             code,
           }, {
